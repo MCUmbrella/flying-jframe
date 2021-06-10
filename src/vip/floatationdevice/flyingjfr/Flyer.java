db8bd21ea6f3c4d4209db513f2f539cdeaeb6089
MCUmbrella <mcumbrella@protonmail.com>
2021-06-10 22:38:00 +0800

    I gave up
    goodbye

--- src/vip/floatationdevice/flyingjfr/Flyer.java
+++ src/vip/floatationdevice/flyingjfr/Flyer.java
@@ -16,24 +16,22 @@ public class Flyer extends JFrame
     private static final long serialVersionUID = 1L; // what
     int sizex=100, sizey=100; // Initial window size
     double x=MAX_X/2-sizex, y=MAX_Y/2-sizey; // Initial window location
-    double addx=1, addy=1; // Base X & Y movement speed
-    static double speedx;// // X acceleration
-    static double speedy;// // Y acceleration
+    double addx=new Random().nextDouble(), addy=new Random().nextDouble(); // Base X & Y movement speed
+    double speedx=new Random().nextDouble()%0.01;// // X acceleration
+    double speedy=new Random().nextDouble()%0.01;// // Y acceleration
     boolean negx=new Random().nextBoolean(), negy=new Random().nextBoolean(); // Controls movement direction
     public void reset()
     {
-        x=new Random().nextDouble()%0.01;
-        y=new Random().nextDouble()%0.01;
         sizex=100; sizey=100;
         x=MAX_X/2-sizex; y=MAX_Y/2-sizey;
-        addx=1; addy=1;
+        addx=new Random().nextDouble(); addy=new Random().nextDouble();
     }
     
     Flyer()
     {
-        reset();
-        if(negx) {speedx*=-1;addx*=-1;}
-        if(negy) {speedy*=-1;addy*=-1;}
+        //reset();
+        //if(negx) {speedx*=-1;addx*=-1;}
+        //if(negy) {speedy*=-1;addy*=-1;}
         setSize(sizex,sizey);
         add(new JButton(""+(new Random().nextDouble()+0)%0.01));
         setLocation((int)x,(int)y);
