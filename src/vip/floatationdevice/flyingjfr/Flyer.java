d3db846def4a814d55c333cf390b2cf134335125
MCUmbrella <mcumbrella@protonmail.com>
2021-06-10 23:10:07 +0800

    lmfao I did it

--- src/vip/floatationdevice/flyingjfr/Flyer.java
+++ src/vip/floatationdevice/flyingjfr/Flyer.java
@@ -16,22 +16,21 @@ public class Flyer extends JFrame
     private static final long serialVersionUID = 1L; // what
     int sizex=100, sizey=100; // Initial window size
     double x=MAX_X/2-sizex, y=MAX_Y/2-sizey; // Initial window location
-    double addx=new Random().nextDouble(), addy=new Random().nextDouble(); // Base X & Y movement speed
-    double speedx=new Random().nextDouble()%0.01;// // X acceleration
-    double speedy=new Random().nextDouble()%0.01;// // Y acceleration
+    double sx=new Random().nextDouble(), sy=new Random().nextDouble(); // Base X & Y movement speed
+    double ax=new Random().nextDouble()%0.01;// // X acceleration
+    double ay=new Random().nextDouble()%0.01;// // Y acceleration
     boolean negx=new Random().nextBoolean(), negy=new Random().nextBoolean(); // Controls movement direction
     public void reset()
     {
-        sizex=100; sizey=100;
+        //sizex=100; sizey=100;
         x=MAX_X/2-sizex; y=MAX_Y/2-sizey;
-        addx=new Random().nextDouble(); addy=new Random().nextDouble();
+        sx=new Random().nextDouble(); sy=new Random().nextDouble();
+        if(new Random().nextBoolean())sx*=-1;if(new Random().nextBoolean())sy*=-1;
     }
     
     Flyer()
     {
-        //reset();
-        //if(negx) {speedx*=-1;addx*=-1;}
-        //if(negy) {speedy*=-1;addy*=-1;}
+        if(new Random().nextBoolean())sx*=-1;if(new Random().nextBoolean())sy*=-1;
         setSize(sizex,sizey);
         add(new JButton(""+(new Random().nextDouble()+0)%0.01));
         setLocation((int)x,(int)y);
@@ -41,9 +40,9 @@ public class Flyer extends JFrame
             public void actionPerformed(ActionEvent ev)
             {
                 if(x>MAX_X*1.5||y>MAX_Y*1.5||x<MAX_X*-1||y<MAX_Y*-1){reset();} // Reset window location & size if it flew out of the screen
-                x+=addx;y+=addy;
-                addx*=(speedx+1);
-                addy*=(speedy+1);
+                x+=sx;y+=sy;
+                sx*=(ax+1);
+                sy*=(ay+1);
                 setLocation((int)x,(int)y);
             }
         };
